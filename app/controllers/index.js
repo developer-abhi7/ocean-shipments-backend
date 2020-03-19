686fa7e8051be7c4cb2acced7eaf7b3bd9a1f6e7
abhijnyan <abhijnyan.devadiga@zincdigital.com>
2020-03-19 18:55:55 +0530

    added users

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,8 +1,10 @@
 const note_ctrl = require('./note.controller');
 const container_ctrl = require('./container.controller');
 const shipment_ctrl = require('./shipment.controller');
+const user_ctrl = require('./users.controller');
 module.exports = {
     note_ctrl,
     container_ctrl,
-    shipment_ctrl
+    shipment_ctrl,
+    user_ctrl
 };
